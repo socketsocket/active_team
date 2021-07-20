9a0c19074ed5423f48e6b60c602b83a056330748
jolim <jolim@student.42seoul.fr>
2021-07-20 17:09:43 +0900

    rough class specification added

---
+++ mapcontainer.cpp
@@ -0,0 +1,11 @@
+template<typaname Key, typename Value, typename Less = std::Less<Key>>
+class map()
+{
+
+pair<Key, Value>	search(Key key)
+{
+	!(a < b) && !(b < a)
+	!(less(a, b)) && !(less(b, a))
+}
+
+}
