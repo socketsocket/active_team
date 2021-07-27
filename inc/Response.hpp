90868058458937b2361363ba0ebcd2572aa73b9a
zittoooo <stellar7484@naver.com>
2021-07-27 20:50:41 +0900

    http status codes 100~300

--- inc/Response.hpp
+++ inc/Response.hpp
@@ -33,6 +33,28 @@ const Message Response::status_message[] = {
 	// 100, 200 jiholee
     // 300, 500 hson
     // 400 seyu
+    {100, "Continue"},
+    {101, "Switching Protocols"},
+    {102, "Processing"},
+    {200, "OK"},
+    {201, "Created"},
+    {202, "Accepted"},
+    {203, "Non-Authoritative Information"},
+    {204, "No Content"},
+    {205, "Reset Content"},
+    {206, "Partial Content"},
+    {207, "Multi-Status"},
+    {208, "Already Reported"},
+    {226, "IM Used"},
+    {300, "Multiple Choices"},
+    {301, "Moved Permanently"},
+    {302, "Found"},
+    {303, "See Other"},
+    {304, "Not Modified"},
+    {305, "Use Proxy"},
+    {307, "Temporary Redirect"},
+    {308, "Permanent Redirect"},
+
 	{400, "Bad Request"}, 
 	{401, "Unauthorized"},
 	{402, "Payment Required"},
